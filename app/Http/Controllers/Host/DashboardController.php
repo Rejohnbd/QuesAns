984488d8bb2662c6afd27c74bdd310d378ba79d6
rejohnbd <rejohn.cse@gmail.com>
2023-07-07 03:02:10 +0600

    Update Dashboard For Host

--- app/Http/Controllers/Host/DashboardController.php
+++ app/Http/Controllers/Host/DashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Host;
 
 use App\Http\Controllers\Controller;
+use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('host.dashboard.index');
+        $assignedQuestions = Question::where('user_id', Auth::user()->id)->where('status', 1)->count();
+        $answeredQuestions = Question::where('user_id', Auth::user()->id)->where('status', 2)->count();
+        return view('host.dashboard.index', compact('assignedQuestions', 'answeredQuestions'));
     }
 }
--- resources/views/host/dashboard/index.blade.php
+++ resources/views/host/dashboard/index.blade.php
@@ -12,22 +12,10 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-4 col-6">
-            <div class="small-box bg-info">
-                <div class="inner">
-                    <h3></h3>
-                    <p>Today Question</p>
-                </div>
-                <div class="icon">
-                    <i class="far fa-question-circle"></i>
-                </div>
-                <a href="" class="small-box-footer">Show Detials <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <div class="col-lg-4 col-6">
+        <div class="col-6">
             <div class="small-box bg-success">
                 <div class="inner">
-                    <h3></h3>
+                    <h3>{{ $answeredQuestions }}</h3>
                     <p>Answered Question</p>
                 </div>
                 <div class="icon">
@@ -37,10 +25,10 @@
             </div>
         </div>
 
-        <div class="col-lg-4 col-6">
+        <div class="col-6">
             <div class="small-box bg-warning">
                 <div class="inner">
-                    <h3></h3>
+                    <h3>{{ $assignedQuestions }}</h3>
                     <p>Unanswered Question</p>
                 </div>
                 <div class="icon">
